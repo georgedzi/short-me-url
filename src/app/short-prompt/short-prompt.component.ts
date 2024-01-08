e11a46b2860a0fbed2859f5211296db356144366
unknown <georgedziouba@gmail.com>
2024-01-07 19:05:26 -0500

    css cleanup. re-arrange controls

--- src/app/short-prompt/short-prompt.component.ts
+++ src/app/short-prompt/short-prompt.component.ts
@@ -1,22 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-short-prompt',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './short-prompt.component.html',
   styleUrl: './short-prompt.component.scss'
 })
 export class ShortPromptComponent {
   public urlFormGroup: FormGroup;
-  public urlPlaceHolder: string = "Super long URL here";
-  public shortenBtnTitle: string = "Shrink";
-  public InvalidUrlError: string = "Invalid URL";
+  public longURLPlaceHolder: string = "Super long URL here";
+  public shortURLPlaceHolder: string = "Short URL outputs here";
+  public submitBtnText: string = "Shrink";
+  public InvalidUrlError: string = "default";
   public submitted: boolean = false;
-  public shortURL: string = "default";
-  public displayShort: boolean = false;
   private urlRegex = new RegExp(
     '^(https?:\\/\\/)?' +
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
@@ -32,16 +31,18 @@ export class ShortPromptComponent {
   }
 
   onSubmit() {
-
     if (this.urlFormGroup.valid) {
       // call backend to save url
       this.submitted = false;
-      this.shortURL = "http://localhost:4200/hashvalue";
-      this.displayShort = true;
     }
     else {
+      if(this.urlFormGroup.controls['longUrl'].errors?.['required']){
+        this.InvalidUrlError = "Required";
+      }
+      else{
+        this.InvalidUrlError = "Invalid URL"
+      }
       this.submitted = true;
-      this.displayShort = false;
     }
   }
 }
