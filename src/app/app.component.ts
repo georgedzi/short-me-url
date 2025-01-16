b354bdad3f6e39c464188b7c43751be4dc1002c1
georgedzi <georgedziouba@gmail.com>
2025-01-15 19:20:59 -0500

    rework css, add dummy test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ShortPromptComponent } from "./short-prompt/short-prompt.component";
+import { TestComponent } from "./test/test.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, ShortPromptComponent]
+    imports: [CommonModule, RouterOutlet, ShortPromptComponent, TestComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
+
+
+  ngOnInit() {
+    this.renderer.listen('window', 'click', (event) => {
+      console.log('Focused element:', event.target);
+    });
+  }
 }
--- src/app/short-prompt/short-prompt.component.ts
+++ src/app/short-prompt/short-prompt.component.ts
@@ -1,48 +1,56 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-short-prompt',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, FormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './short-prompt.component.html',
   styleUrl: './short-prompt.component.scss'
 })
 export class ShortPromptComponent {
   public urlFormGroup: FormGroup;
-  public longURLPlaceHolder: string = "Super long URL here";
-  public shortURLPlaceHolder: string = "Short URL outputs here";
-  public submitBtnText: string = "Shrink";
-  public InvalidUrlError: string = "default";
+  public urlPlaceHolder: string = "Enter URL...";
+  public shortenBtnTitle: string = "Shrink";
+  public InvalidUrlError: string = "Invalid URL";
   public submitted: boolean = false;
+  public shortURL: string = "Default";
+  public displayShort: boolean = false;
   private urlRegex = new RegExp(
-    '^(https?:\\/\\/)?' +
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-    '((\\d{1,3}\\.){3}\\d{1,3}))' +
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-    '(\\?[;&a-z\\d%_.~+=-]*)?' +
-    '(\\#[-a-z\\d_]*)?$',
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', // fragment locator
     'i'
   );
 
   constructor() {
-    this.urlFormGroup = new FormGroup({ longUrl: new FormControl("", [Validators.required, Validators.pattern(this.urlRegex)]) });
+    this.urlFormGroup = new FormGroup({ longUrl: new FormControl("", [Validators.required, Validators.pattern(this.urlRegex)])});
   }
 
   onSubmit() {
+
     if (this.urlFormGroup.valid) {
       // call backend to save url
       this.submitted = false;
+      this.shortURL = "http://localhost:4200/hashvalue";
+      this.displayShort = true;
     }
     else {
-      if(this.urlFormGroup.controls['longUrl'].errors?.['required']){
-        this.InvalidUrlError = "Required";
-      }
-      else{
-        this.InvalidUrlError = "Invalid URL"
-      }
       this.submitted = true;
+      this.displayShort = false;
+    }
+  }
+
+  isValidUrl(url: string) {
+    try {
+      new URL(url);
+      return true;
+    } catch (err) {
+      return false;
     }
   }
 }
